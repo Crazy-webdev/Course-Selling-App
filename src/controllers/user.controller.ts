99a6229e5d43a7912bed08913fe603588d44f9f8
Aryan Bhat <aryanbhat2527@gmail.com>
2025-03-08 11:53:12 +0530

    Removed redundant import

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,4 @@
-import { Request, Response } from "express"
-import express from 'express';
+import { Request, Response } from "express";
 
 const signup = (req:Request, res: Response ) => {
     res.send('signup');
