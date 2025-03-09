73e8c0c76c1666019d48aecf2abab7198249b3c8
Aryan Bhat <aryanbhat2527@gmail.com>
2025-03-09 18:29:51 +0530

    Linting and run command addded

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,24 +1,19 @@
 import { Request, Response } from "express";
 
-const adminSignup = (req:Request, res: Response ) => {
-    res.send('signup');
-}
+const adminSignup = (req: Request, res: Response) => {
+  res.send("signup");
+};
 
-const adminSignin = (req:Request, res: Response ) => {
-    res.json({ msg : 'signin'});
-}
+const adminSignin = (req: Request, res: Response) => {
+  res.json({ msg: "signin" });
+};
 
-const adminCourse = (req:Request, res: Response ) => {
-    res.json({ msg : 'adminCourse'});
-}
+const adminCourse = (req: Request, res: Response) => {
+  res.json({ msg: "adminCourse" });
+};
 
-const allCourses = (req:Request, res: Response ) => {
-    res.json({ msg : 'allCourses'});
-}
+const allCourses = (req: Request, res: Response) => {
+  res.json({ msg: "allCourses" });
+};
 
-export {
-    adminSignin,
-    adminSignup,
-    adminCourse,
-    allCourses
-}
+export { adminSignin, adminSignup, adminCourse, allCourses };
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,12 +1,17 @@
-import express from 'express';
-import { adminCourse, adminSignin, adminSignup, allCourses } from '../controllers/admin.controllers';
+import express from "express";
+import {
+  adminCourse,
+  adminSignin,
+  adminSignup,
+  allCourses,
+} from "../controllers/admin.controller";
 
 const adminRouter = express.Router();
 
-adminRouter.post('/signup', adminSignup);
-adminRouter.get('/signin', adminSignin);
-adminRouter.post('/course',adminCourse);
-// adminRouter.put('/course',adminCourse);       
-adminRouter.get('/course/bulk',allCourses);
+adminRouter.post("/signup", adminSignup);
+adminRouter.get("/signin", adminSignin);
+adminRouter.post("/course", adminCourse);
+// adminRouter.put('/course',adminCourse);
+adminRouter.get("/course/bulk", allCourses);
 
 export default adminRouter;
